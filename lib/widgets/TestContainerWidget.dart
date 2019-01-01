e930a74e82888c9f0724335db54d3d15397f71fd
xiangzhenbiao <xiangzhenbiao@fangdd.com>
2019-01-01 11:30:07 +0800

    文本居中显示

--- lib/widgets/TestContainerWidget.dart
+++ lib/widgets/TestContainerWidget.dart
@@ -10,10 +10,14 @@ class TestContainerWidget extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      child: Text('Container text'),
+      // 把文本放在 Container 的中间
+      child: Center(
+        child: Text('Container text'),
+      ),
       padding: EdgeInsets.all(5.0),
       margin: EdgeInsets.all(15.0),
-      width: 180.0,
+      width: 200.0,
+      height: 200.0,
       decoration: BoxDecoration(
         color: Colors.blue,
         borderRadius: BorderRadius.circular(5.0),
